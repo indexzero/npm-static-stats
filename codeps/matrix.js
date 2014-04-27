032be51e267d21d5b0a4e6ae6781bea072a3b002
indexzero <charlie.robbins@gmail.com>
2014-04-27 05:33:56 -0400

    [doc] Added code documentation for matrix.js

--- codeps/matrix.js
+++ codeps/matrix.js
@@ -1,13 +1,36 @@
+/*
+ * matrix.js: Calculates the base data necessary for a co-depencency matrix.
+ *
+ * (C) 2014 Charlie Robbins
+ *
+ */
+
 var async = require('async'),
     weights = require('./weights');
 
 //
+// ### function weights (options, callback)
+// #### @options {Object}
+// ####   - package  {string}   Package to calculate weights for
+// ####   - registry {string}   Registry to calculate against
+// ####   - top      {number}   **Optional** If set, returns only the top N codeps
+// ####   - filter   {function} **Optional** If set, returns only those matching the `filter`.
+// ####   - view     {string}   **Optional** View to calculate weights against.
 //
+// Calculates the weights for the specified `package` against the `view` in the
+// `registry` provided along with the same weights for every package in the initial
+// set filtering only those in that set. These two pieces of data constitute all data
+// necessary to fully represent the full measure of the codependency
+// relationship of the `package`.
 //
 var matrix = module.exports = function (options, callback) {
   weights(options, function (err, relate) {
-    if (err) { return callback(err) }
+    if (err) { return callback(err); }
 
+    //
+    // Remark (indexzero): Not crazy about these inline functions,
+    // but it gets the job done.
+    //
     var codeps = Object.keys(relate).filter(function (n) { return n !== 'total' }),
         lookup = codeps.reduce(function (a, n) { a[n] = 1; return a }, {}),
         rows   = {};
@@ -18,6 +41,7 @@ var matrix = module.exports = function (options, callback) {
         weights({
           package:  name,
           registry: options.registry,
+          view:     options.view,
           filter:   function (val) {
             return !!lookup[val];
           }
